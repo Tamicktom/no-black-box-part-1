421441990d32f89ce4b23f0d666770a0d4a82a69
Henrique Fonseca <rickpokemon38@gmail.com>
2023-12-29 18:49:31 -0300

    Add undo functionality to sketchPad

--- web/src/pages/_scripts/sketchPad.ts
+++ web/src/pages/_scripts/sketchPad.ts
@@ -5,6 +5,9 @@ class SketchPad {
   ctx = this.canvas.getContext("2d")!;
   #isDrawing = false;
   paths: NumberTuple[][] = [];
+
+  undoBtn = document.createElement("button");
+
   constructor(container: HTMLElement, size = 512) {
     this.canvas.width = size;
     this.canvas.height = size;
@@ -13,6 +16,12 @@ class SketchPad {
     this.canvas.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.2)";
 
     container.appendChild(this.canvas);
+
+    const lineBreak = document.createElement("br");
+    container.appendChild(lineBreak);
+
+    this.undoBtn.innerText = "Undo";
+    container.appendChild(this.undoBtn);
 
     this.#isDrawing = false;
     this.paths = [];
@@ -64,11 +73,42 @@ class SketchPad {
     this.canvas.ontouchend = () => {
       this.canvas.dispatchEvent(new MouseEvent("mouseup", {}));
     };
+
+    this.undoBtn.addEventListener("click", () => {
+      this.paths.pop();
+      this.#redraw();
+    });
+
+    this.undoBtn.classList.add(
+      "px-4",
+      "py-2",
+      "bg-gray-200",
+      "rounded-lg",
+      "shadow-md",
+      "hover:bg-gray-300",
+      "focus:outline-none",
+      "focus:ring-2",
+      "focus:ring-gray-400",
+      "focus:ring-opacity-50",
+      "disabled:opacity-50",
+      "disabled:cursor-not-allowed",
+      "disabled:shadow-none",
+      "disabled:bg-gray-200",
+      "disabled:hover:bg-gray-200",
+      "transition-all",
+      "duration-200"
+    );
   }
 
   #redraw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     draw.paths(this.ctx, this.paths);
+
+    if (this.paths.length === 0) {
+      this.undoBtn.disabled = true;
+    } else {
+      this.undoBtn.disabled = false;
+    }
   }
 
   #getMousePosition(e: MouseEvent | TouchEvent): NumberTuple {
