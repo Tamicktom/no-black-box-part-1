a57ec37e746ae44c8de92e70c384e2ba7e6c9f12
Henrique Fonseca <rickpokemon38@gmail.com>
2023-12-31 02:21:42 -0300

    Add dependencies and update dataset generator

--- server/src/dataset_generator.ts
+++ server/src/dataset_generator.ts
@@ -1,17 +1,37 @@
 //* Libraries imports
-import { t } from "elysia";
 import fs from "node:fs";
 
 //* Local imports
 import constants from "./constants";
+import { dataSchemaZod } from "./schemas/raw";
+
+type Sample = {
+  id: number;
+  label: string;
+  student_name: string;
+  student_id: number;
+};
 
 const fileNames = fs.readdirSync(constants.RAW_DIR);
-const samples = [];
+const samples: Sample[] = [];
 let id = 1;
 
 fileNames.map((fileName) => {
   //process only .json files
   if (fileName.includes(".json")) {
-    console.log(`Processing ${fileName}...`);
+    const content = fs.readFileSync(constants.RAW_DIR + "/" + fileName);
+    const data = JSON.parse(content.toString());
+    const { session, drawings, student } = dataSchemaZod.parse(data);
+    for (let label in drawings) {
+      samples.push({
+        id,
+        label,
+        student_name: student,
+        student_id: session,
+      });
+      id++;
+    }
   }
 });
+
+fs.writeFileSync(constants.SAMPLES, JSON.stringify(samples));
--- server/src/schemas/raw.ts
+++ server/src/schemas/raw.ts
@@ -1,5 +1,6 @@
 //* Libraries imports
 import { t } from "elysia";
+import z from "zod";
 
 export const dataSchema = t.Object({
   student: t.String(),
@@ -8,6 +9,17 @@ export const dataSchema = t.Object({
     t.Object({
       label: t.String(),
       paths: t.Array(t.Array(t.Array(t.Number()))),
+    })
+  ),
+});
+
+export const dataSchemaZod = z.object({
+  student: z.string(),
+  session: z.number(),
+  drawings: z.array(
+    z.object({
+      label: z.string(),
+      paths: z.array(z.array(z.array(z.number()))),
     })
   ),
 });
